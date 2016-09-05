cf3cd162ff8464b70c4b857a8f862670cfc733be
Phil Davis <phil.davis@inf.org>
2016-09-05 13:20:05 +0930

    Make a basic Wordgroup
    Constructor to make a new WordGroup, putting just a single word into a
    linked list. Calculate the lowercase sorted value of the given "word"
    and store that. That is the "key" that should be the same for all words
    allowed in the group.
    Add a method to return the key for a group.
    Add tests that create WordGroup(s) with various combinations of
    characters in the initial word, and check that the WordKey is sorted as
    expected.

--- src/WordGroup/TestWordGroup.java
+++ src/WordGroup/TestWordGroup.java
@@ -26,9 +26,114 @@ public class TestWordGroup {
 	public void tearDown() throws Exception {
 	}
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+    @Test
+    public void checkSortedKeyabc() {
+        System.out.println("Checking sorted key abc");
+
+        WordGroup instance = new WordGroup("abc");
+        String expResult = "abc";
+        String result = instance.getWordKey();
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void checkSortedKeycba() {
+        System.out.println("Checking sorted key cba");
+
+        WordGroup instance = new WordGroup("cba");
+        String expResult = "abc";
+        String result = instance.getWordKey();
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void checkSortedKeyABC() {
+        System.out.println("Checking sorted key ABC");
+
+        WordGroup instance = new WordGroup("ABC");
+        String expResult = "abc";
+        String result = instance.getWordKey();
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void checkSortedKeyCBA() {
+        System.out.println("Checking sorted key CBA");
+
+        WordGroup instance = new WordGroup("CBA");
+        String expResult = "abc";
+        String result = instance.getWordKey();
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void checkSortedKeya() {
+        System.out.println("Checking sorted key a");
+
+        WordGroup instance = new WordGroup("a");
+        String expResult = "a";
+        String result = instance.getWordKey();
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void checkSortedKeyA() {
+        System.out.println("Checking sorted key A");
+
+        WordGroup instance = new WordGroup("A");
+        String expResult = "a";
+        String result = instance.getWordKey();
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void checkSortedKeyNull() {
+        System.out.println("Checking sorted key Null");
+
+        WordGroup instance = new WordGroup("");
+        String expResult = "";
+        String result = instance.getWordKey();
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void checkSortedKeyMixed1() {
+        System.out.println("Checking sorted key mixed 1");
+
+        WordGroup instance = new WordGroup("QwErTy");
+        String expResult = "eqrtwy";
+        String result = instance.getWordKey();
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void checkSortedKeyMixed2() {
+        System.out.println("Checking sorted key mixed 1");
+
+        WordGroup instance = new WordGroup("qWeRtY");
+        String expResult = "eqrtwy";
+        String result = instance.getWordKey();
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void checkSortedKeyNumeric() {
+        System.out.println("Checking sorted key numeric");
+
+        WordGroup instance = new WordGroup("5y32X9a");
+        String expResult = "2359axy";
+        String result = instance.getWordKey();
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void checkSortedKeyRepeat() {
+        System.out.println("Checking sorted key repeat");
+
+        WordGroup instance = new WordGroup("t588Yftc9y5k5");
+        String expResult = "555889cfkttyy";
+        String result = instance.getWordKey();
+        assertEquals(expResult, result);
+    }
 
 }
--- src/WordGroup/WordGroup.java
+++ src/WordGroup/WordGroup.java
@@ -1,5 +1,30 @@
 package WordGroup;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+
 public class WordGroup {
 
+    // -------------------------
+    // Class Attributes 
+    // -------------------------
+    private LinkedList<String> ll = new LinkedList<String>();
+    private String WordKey;
+
+    // -------------------------
+    // Constructors
+    // -------------------------
+    public WordGroup(String s1) {
+        this.ll.add(s1);
+        char[] chararray = s1.toLowerCase().toCharArray();
+        Arrays.sort(chararray);
+        this.WordKey = String.valueOf(chararray);
+    }
+    
+    // -------------------------
+    // Methods
+    // -------------------------
+    public String getWordKey() {
+    	return this.WordKey;
+    }
 }
