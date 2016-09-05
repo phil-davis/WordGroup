db3d9a15a9cf7642392f9815e6286956575d8f60
Phil Davis <phil.davis@inf.org>
2016-09-05 18:21:50 +0930

    Provide real main to process user input
    Read from standard input until the user enters a blank line.
    Add the word to the first WordGroup that matches, if none match then
    create a new WordGroup.
    Sort the linked list by the wordCount of each entry - BigWordCountFirst
    Loop through each WordGroup and print them out.

--- src/WordGroup/Main.java
+++ src/WordGroup/Main.java
@@ -1,14 +1,62 @@
 package WordGroup;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.ListIterator;
+import java.util.Scanner;
+
+class BigWordCountFirst implements Comparator<Object> {@Override
+public int compare(Object arg0, Object arg1) {
+	WordGroup x = (WordGroup)arg0;
+	WordGroup y = (WordGroup)arg1;
+	// Bigger wordCount is required to sort first
+	if(x.wordCount() > y.wordCount()) {
+		return -1;
+	} else if(x.wordCount() == y.wordCount()) {
+		return 0;
+	} else {
+		return 1;
+	}
+}}
+
 public class Main {
     public static void main(String[] args) {
-        WordGroup wg = new WordGroup("How");
-        Boolean result = wg.addWord("Who");
-        if (result) {
+        LinkedList<WordGroup> ll = new LinkedList<WordGroup>();
+        Scanner scan = new Scanner(System.in);
+        
+        while (true) {
+	        System.out.println("Enter a word (or RETURN to finish): ");
+	        String s = scan.nextLine();
+	        if (s.equals("")) {
+	        	break;
+	        }
+	        
+            ListIterator<WordGroup> listIterator = ll.listIterator();
+        	Boolean wordAdded = false;
+        	while (listIterator.hasNext()) {
+        		wordAdded = listIterator.next().addWord(s);
+        		if (wordAdded) {
+        			break;
+        		}
+    		}
+        	
+	        if (!wordAdded) {
+	            WordGroup newGroup = new WordGroup(s);
+	            ll.add(newGroup);
+	        }
+	    };
+        
+        System.out.println("Word groups and counts:");
+
+        Collections.sort(ll, new BigWordCountFirst());
+        
+        ListIterator<WordGroup> listIterator2 = ll.listIterator();
+    	while (listIterator2.hasNext()) {
+    		WordGroup wg = listIterator2.next();
         	System.out.println(wg.wordCount() + ": " + wg.wordListAsString());
-        } else {
-        	System.out.println("Error in addWord");
-        }
+		}
+        scan.close();
     }
 
 }
